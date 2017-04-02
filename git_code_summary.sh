6de2ab22c3bdcebeb3f69bf6eacf5abc5c9fd731
Hai Yang <h6yang@ucsd.edu>
2017-04-02 09:38:46 -0700

    add git summary code

---
+++ git_code_summary.sh
@@ -0,0 +1,37 @@
+# some basic code for github
+
+# cd to the directory you want the code stay on your local
+cd /Users/hai/Documents/Personal/Data\ Scientist\ Learning/git
+
+git init
+
+git status
+
+# on the github, create a repo called datasciencecoursera
+
+git remote add origin https://github.com/h6yang/datasciencecoursera.git
+
+# create a file in you local
+vi HelloWorld.md
+
+# add your file to git local
+git add HelloWorld.md 
+
+# commit you file
+git commit -m "Add HelloWorld.md"
+
+# check the log file by 
+git log
+
+# add your file from your local git to github web
+git remote add datasciencecoursera https://github.com/h6yang/datasciencecoursera.git
+
+# push to github web
+git push -u datasciencecoursera master
+git push -f -u datasciencecoursera master
+
+
+# clone other's github to your local
+# on other's github page, "fork" the repo; Then this repo will show on your github
+# after fork other's repo, clone to your local git 
+git clone https://github.com/h6yang/datasharing.git
